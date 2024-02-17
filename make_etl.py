e336fcaf3f6eda930e6c45ac469bcd357cb68482
Coumba KANOUTE <kanoute.coumba@gmail.com>
2024-02-17 10:28:42 +0000

    another update

--- make_etl.py
+++ make_etl.py
@@ -1,27 +1,77 @@
+# coding: utf-8
+# Importation des bibliothèques nécessaires
+
+import os
+import zipfile
+import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when, avg, dense_rank
+import kaggle
+from pyspark.sql.functions import when, avg, col
 from pyspark.sql.window import Window
-from pyspark import SparkFiles
 
-# Initialisation la session Spark
-spark = SparkSession.builder.appName("AnalyseRetardsVols").getOrCreate()
+# Répertoire contenant la clé API kaggle.json
+os.environ['KAGGLE_CONFIG_DIR'] = '~/.kaggle/kaggle.json'
 
-# Ajout du fichier token Kaggle à l'environnement Spark
-chemin_token_kaggle = "/home/hadoop/.kaggle/kaggle.json"
-spark.sparkContext.addFile("file://" + chemin_token_kaggle)
+# Configurons la session Spark
+spark = SparkSession.builder.appName("FlightDelaysETL").getOrCreate()
+spark.conf.set("fs.defaultFS", "hdfs://192.168.1.100:9000")
+os.environ['HADOOP_CONF_DIR'] = '$HADOOP_HOME/etc/hadoop'
+os.environ['YARN_CONF_DIR'] = '$HADOOP_HOME/etc/hadoop'
 
-# Chargement et exploration des données depuis Kaggle
-kaggle_dataset_url = "kaggle datasets download -d usdot/flight-delays -p /content"
-spark.sparkContext.addFile(kaggle_dataset_url)
-donnees_vols = spark.read.csv("file://" + SparkFiles.get("flight-delays.zip"), header=True, inferSchema=True)
+# L'URL du dataset Kaggle
+kaggle_dataset_url = "usdot/flight-delays"
 
-# Affichage les 10 premières lignes et imprimer le schéma pour comprendre la structure des données
-donnees_vols.show(10)
-donnees_vols.printSchema()
+# Chemin local d'extraction
+local_extract_path = '/home/hadoop/spark-test/dataset'
+
+# Téléchargement du dataset en tant que Pandas DataFrame
+kaggle.api.dataset_download_files(dataset=kaggle_dataset_url, unzip=True, quiet=False, path=local_extract_path)
+
+# Liste des fichiers extraits
+extracted_files = os.listdir(local_extract_path)
+
+# Initialisation des DataFrames Spark
+airlines_df = None
+airports_df = None
+flights_df = None
+
+# Lire chaque fichier CSV et charger dans le DataFrame Spark correspondant
+for file_name in extracted_files:
+    csv_file_path = os.path.join(local_extract_path, file_name)
+
+    if "airlines" in file_name:
+        airlines_df = spark.read.csv("file://" + csv_file_path, header=True, inferSchema=True)
+    elif "airports" in file_name:
+        airports_df = spark.read.csv("file://" + csv_file_path, header=True, inferSchema=True)
+    elif "flights" in file_name:
+        flights_df = spark.read.csv("file://" + csv_file_path, header=True, inferSchema=True)
+
+# Chemin dans HDFS où vous souhaitez sauvegarder les DataFrames
+hdfs_path = 'hdfs://192.168.1.100:9000/'
+
+# Copie des fichiers vers HDFS
+for file_name in extracted_files:
+    local_file_path = os.path.join(local_extract_path, file_name)
+    hdfs_file_path = hdfs_path + file_name
+    os.system(f'hdfs dfs -copyFromLocal {local_file_path} {hdfs_file_path}')
+
+# Sauvegarde des DataFrames dans HDFS
+airlines_df.write.csv(hdfs_path + 'airlines')
+airports_df.write.csv(hdfs_path + 'airports')
+flights_df.write.csv(hdfs_path + 'flights')
+
+# Chargement des DataFrames depuis HDFS
+airlines_df = spark.read.csv(hdfs_path + 'airlines', header=True, inferSchema=True)
+airports_df = spark.read.csv(hdfs_path + 'airports', header=True, inferSchema=True)
+flights_df = spark.read.csv(hdfs_path + 'flights', header=True, inferSchema=True)
+
+# Affichons le schéma et quelques lignes du DataFrame des vols
+flights_df.printSchema()
+flights_df.show(10)
 
 # Nettoyage des données avec l'API DataFrame
-donnees_vols = donnees_vols.withColumn("retardé", when(col("ARRIVAL_DELAY") > 15, 1).otherwise(0))
-donnees_vols = donnees_vols.fillna(0, subset=["ARRIVAL_DELAY"])  # pour gerer les valeurs manquantes
+donnees_vols = flights_df.withColumn("retardé", when(col("ARRIVAL_DELAY") > 15, 1).otherwise(0))
+donnees_vols = donnees_vols.fillna(0, subset=["ARRIVAL_DELAY"])  # pour gérer les valeurs manquantes
 
 # Agrégation et regroupement des données
 retard_moyen_par_compagnie = donnees_vols.groupBy("AIRLINE").agg(avg("ARRIVAL_DELAY").alias("retard_moyen"))
