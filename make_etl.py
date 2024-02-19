74aa308b676832959945f0cf8b30eaf2b44603b6
kanoute-coumba <kanoute.coumba@gmail.com>
2024-02-19 19:57:57 +0100

    troubleshootings

--- make_etl.py
+++ make_etl.py
@@ -13,12 +13,15 @@ spark = SparkSession.builder.appName("Analyse des retards de vol").getOrCreate()
 
 # Chemin complet du fichier CSV
 csv_path = os.path.join(os.getcwd(), dataset_path)
-# Chargeons le fichier CSV dans un DataFrame Spark
-df = spark.read.csv(csv_path, header=True, inferSchema=True)
+
 ###################################################################
 print("Chemin complet du fichier CSV :", csv_path)
 print("Répertoire de travail actuel :", os.getcwd())
 ###################################################################
+
+# Chargeons le fichier CSV dans un DataFrame Spark
+df = spark.read.csv(csv_path, header=True, inferSchema=True)
+
 # Chargeons le jeu de données dans un DataFrame Spark
 # df = spark.read.csv("datasets/flights.csv", header=True, inferSchema=True)
 
